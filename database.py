58b43f917338ff1807ad944465b41f46ef705dc0
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2024-12-30 21:27:52 -0500

    Feat: Fake products were created

---
+++ database.py
@@ -0,0 +1,32 @@
+products = [
+    {
+        "id": "1",
+        "name": "laptop",
+        "price": 800,
+        "quantity": 4
+    },
+    {
+        "id": "2",
+        "name": "smartphone",
+        "price": 500,
+        "quantity": 10
+    },
+    {
+        "id": "3",
+        "name": "headphones",
+        "price": 100,
+        "quantity": 15
+    },
+    {
+        "id": "4",
+        "name": "keyboard",
+        "price": 50,
+        "quantity": 20
+    },
+    {
+        "id": "5",
+        "name": "monitor",
+        "price": 300,
+        "quantity": 8
+    }
+]
